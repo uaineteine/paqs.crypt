b0c3eb7219b9d03d09c962e2f7e3bf2ebe94de70
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-08-31 11:39:52 +1000

    working test program/method

---
+++ paqs.testcrypt/Program.cs
@@ -0,0 +1,23 @@
+﻿using paqs.crypt;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        string password = "your_password";
+        byte[] salt = Encoding.UTF8.GetBytes("your_salt");
+        byte[] iv = AesEncryption.GenerateRandomIV();
+        byte[] data = Encoding.UTF8.GetBytes("Hello, World!");
+
+        // Encrypt the data
+        byte[] encryptedData = AesEncryption.Encrypt(data, password, salt, iv);
+        Console.WriteLine("Encrypted Data: " + Convert.ToBase64String(encryptedData));
+
+        // Decrypt the data
+        byte[] decryptedData = AesEncryption.Decrypt(encryptedData, password, salt, iv);
+        Console.WriteLine("Decrypted Data: " + Encoding.UTF8.GetString(decryptedData));
+    }
+}
